a0e65deb63b67e9dca40232ba0c19659bd81827b
carlos19450 <infocarlosexpuig@gmail.com>
2024-03-30 03:57:11 +0100

    Se han agregado animaciones al personaje, se ha creado parte del mapa inicial, se han añadido assets para usar durante el desarrollo

--- Assets/Scripts/PersonajeController.cs
+++ Assets/Scripts/PersonajeController.cs
@@ -8,17 +8,30 @@ public class PersonajeController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private Rigidbody2D rb;
+    private Animator runAnimator;
     private bool enElSuelo;
+    private bool isMoving;
+    private bool isJumping;
+    private bool isAtacking;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        runAnimator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        runAnimator.SetBool("IsMoving", isMoving);
+        runAnimator.SetBool("IsJumping", isJumping);
         if (Input.GetKeyDown(KeyCode.Space) && enElSuelo)
         {
             Saltar();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(PlayOneShot());
         }
     }
 
@@ -36,6 +49,7 @@ public class PersonajeController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Suelo"))
         {
+            isJumping = false;
             enElSuelo = true;
         }
     }
@@ -46,24 +60,53 @@ public class PersonajeController : MonoBehaviour
         {
             enElSuelo = false;
         }
+
+        if (other.gameObject.CompareTag("Suelo") && Input.GetKey(KeyCode.Space))
+        {
+            isJumping = true;
+            isMoving = false;
+        }
     }
 
     void Moverse()
     {
+        if (!(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)))
+        {
+            isMoving = false;
+        }
         if (Input.GetKey(KeyCode.D))
         {
+            if (!isJumping)
+            {
+                isMoving = true;
+            }
             transform.position += Vector3.right * speed * Time.deltaTime;
-        
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (Input.GetKey(KeyCode.A))
         {
+            if (!isJumping)
+            {
+                isMoving = true;
+            }
             transform.position += Vector3.right * -speed * Time.deltaTime;
-        
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.up * -speed * Time.deltaTime;
-
         }
+    }
+
+    private IEnumerator PlayOneShot ()
+    {
+        runAnimator.SetBool("IsAtacking", true);
+        yield return new WaitForSeconds(1.20f);
+        runAnimator.SetBool("IsAtacking", false);
+    }
+    
+    void Atacar()
+    {
+            isAtacking = true;
     }
 }
